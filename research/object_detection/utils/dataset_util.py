d4510a78071da8d717cc1fb3a07fd74a016f0de4
Klaus-Dieter Mueller <klaus-dieter.mueller@smithsdetection.com>
2018-05-23 15:36:19 +0200

    fixed input pipeline by using all data, shuffled for each epoch

--- research/object_detection/utils/dataset_util.py
+++ research/object_detection/utils/dataset_util.py
@@ -127,14 +127,14 @@ def read_dataset(file_read_func, decode_func, input_files, config):
     tf.logging.warning('`shuffle` is false, but the input data stream is '
                        'still slightly shuffled since `num_readers` > 1.')
 
-  filename_dataset = filename_dataset.repeat(config.num_epochs or None)
-
   records_dataset = filename_dataset.apply(
       tf.contrib.data.parallel_interleave(
           file_read_func, cycle_length=config.num_readers,
           block_length=config.read_block_length, sloppy=config.shuffle))
   if config.shuffle:
     records_dataset = records_dataset.shuffle(config.shuffle_buffer_size)
+  records_dataset = records_dataset.repeat(config.num_epochs or None)
+
   tensor_dataset = records_dataset.map(
       decode_func, num_parallel_calls=config.num_parallel_map_calls)
   return tensor_dataset.prefetch(config.prefetch_size)
